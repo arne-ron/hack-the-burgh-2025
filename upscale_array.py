f839b979b20806068f314833b5dd70f3a6d56b82
arne-ron <arne.roenneberg@gmail.com>
2025-03-01 23:10:27 +0000

    deleted old comment

--- upscale_array.py
+++ upscale_array.py
@@ -1,16 +1,6 @@
 from PIL import Image
 import numpy as np
 
-# def 
-#     newArray = []
-#     for i in range(len(arr)):  # Iterate over rows
-#         for _ in range(3):  # Repeat each row 3 times (vertical scaling)
-#             row = []
-#             for k in range(len(arr[i])):  # Iterate over columns
-#                 row.extend([arr[i][k]] * 3)  # Repeat each [R, G, B] triplet 3 times (horizontal scaling)
-#             newArray.append(row)  # Append the expanded row
-#     return np.array(newArray, dtype=np.uint8)  # Convert back to NumPy array
-
 
 
 def upscaleArray(arr):
