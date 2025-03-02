4b4b7f6c3d82b9fd4361230ad2153fe37d79612c
arne-ron <arne.roenneberg@gmail.com>
2025-03-02 10:55:11 +0000

    fixed main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 from run_kernel import run_kernelRGB, run_kernel1D, run_kernel1D_arr
 #from upscale_array import upscaleArray, upscaleArrayNew
-from landuse_objects import landuse_objects
+from landuse_objects import landuse_objects, combineLanduseMaps
 from normalize import normalize
 
 
@@ -39,6 +39,7 @@ normalized_LiDAR = Image.open(input_path_normalized_LiDAR)
 
 start_time = time.time()
 sat_img = Image.open(input_path_sat)
+sat_2_img = Image.open(input_path_sat_2)
 LiDAR_img = Image.open(input_path_LiDAR)
 if RUN_REDUCED:
     sat_img = sat_img.resize((100, 100))
@@ -46,13 +47,15 @@ if RUN_REDUCED:
 array = np.array(sat_img)
 LiDAR_arr = np.array(LiDAR_img)
 
-gradient_arr = get_gradient(LiDAR_arr)
+# gradient_arr = get_gradient(LiDAR_arr)
 
-print("Processing...")
-new_image_arr = upscale_array(array, gradient_arr, LiDAR_arr, threshold=10)
-img_res = Image.fromarray(new_image_arr, "RGB")
-img_res.save(output_path_sat)
-img_res.show()
+# print("Processing...")
+# new_image_arr = upscale_array(array, gradient_arr, LiDAR_arr, threshold=10)
+# img_res = Image.fromarray(new_image_arr, "RGB")
+# img_res.save(output_path_sat)
+# img_res.show()
+# end_time = time.time()
+# print(f"Done in {end_time - start_time} seconds")
 
 
 # # Upscale the image - from 1 pixel to 10 pixels
@@ -66,10 +69,9 @@ img_res.show()
 
 landuseLiDARImage = np.array(normalized_LiDAR.resize((1000,1000)))
 landuseSatImage = np.array(sat_img.resize((1000,1000)))
-landuseRedImage = np.array(sat_2.resize((1000,1000)))
+landuseRedImage = np.array(sat_2_img.resize((1000,1000)))
 
 landuse_objects(landuseLiDARImage, landuseSatImage, landuseRedImage)
-end_time = time.time()
-print(f"Done in {end_time - start_time} seconds")
+combineLanduseMaps(landuseLiDARImage)
 
 
